dd4231beedc9b7ccdd0bdac02ecb9c407363443e
astles <66706743+astles@users.noreply.github.com>
2022-10-07 01:28:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import streamlit as st
 st.set_page_config(page_title="Streamlit projects", page_icon=None, layout="centered", initial_sidebar_state="expanded", menu_items=None)
 
 st.header("QR CODE MAKER")
+st.sidebar.success("Select a demo above.")
 st.info("Make QRs for Devices, & Users")
 st.image("https://media.giphy.com/media/16dIgjWQjikY8/giphy.gif")
 
